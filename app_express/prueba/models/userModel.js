cd17075094f1f7a76f75829b753208bbb650b341
Rocio Romero <rocio.romero@babel.es>
2016-03-09 12:17:08 +0100

    Ejemplo de un modelo
    MODELO: AQUÍ ESTARÁN LOS DATOS
    
    CONTROLADOR: COGE EL MODELO Y DE LO DA A LA VISTA
    
    VISTA: USA ESO QUE LE HEMOS DADO PARA PINTARLO

---
+++ app_express/prueba/models/userModel.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// AQUÍ ESTARÁN MIS DATOS
+
+
+var users = [{name : 'Smith', age: 30}, 
+			{name: 'Jones', age: 22}
+];
+
+module.exports = users; 
+
+//YA TENGO UN MODELO :)
--- app_express/prueba/routes/users.js
+++ app_express/prueba/routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/userModel');
+
+console.log('users: ', users); // para ver que lo hago bien
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/form', function(req, res, next) {
-  res.render('user_form', {name: 'Smith', age: 30});
+  res.render('user_form', {users : users}); // le paso un objeto con una propiedad que dentro tiene mi objeto que le paso 
 });
 
 
