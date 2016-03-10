e751f712d9c28c8f4ecf6f81cc49130249d55f6a
Rocio Romero <rocio.romero@babel.es>
2016-03-10 10:20:58 +0100

    Conectamos con la base de datos y extraemos la información de ahí

--- app_express/prueba/models/userModel.js
+++ app_express/prueba/models/userModel.js
@@ -2,17 +2,25 @@
 
 // AQUÍ ESTARÁN MIS DATOS
 
+var conn = require('../lib/connectMongo');
 
-var users = [{name : 'Smith', age: 30}, 
-			{name: 'Jones', age: 22}
-];
+
 
 var user = {
 	getUsers: function(cb){ //cb=callback
 		//imaginamos que lee un fichero
-		var usuariosLeidos = users;
-		cb(null, users); //no return sino callback, estoy en asincrono
+		//var usuariosLeidos = users;
+
+		 conn.db.collection('agentes').find({}).toArray(function(err, usuariosLeidos) {
+			 if (err) {
+			 	cb(err);
+			 	return;
+			 }
+			 console.dir(usuariosLeidos);
+			 cb(null, usuariosLeidos); //no return sino callback, estoy en asincrono
+			 return;
 		//return users;
+		 });
 	}
 };
 
