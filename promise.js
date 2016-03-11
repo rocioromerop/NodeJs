51551bf5cb1c1773fba79d7fa9149bc8dc867c07
Rocio Romero <rocio.romero@babel.es>
2016-03-11 12:07:33 +0100

    Promesas (1)

---
+++ promise.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var sleep = function(ms){
+
+	var ms = ms || 1000; //si no me dan ms, pon 1000
+
+	var promesa = new Promise (function (resolve, reject){
+
+		setTimeout(function(){
+			if(ms % 2 !== 0){ //equivale al if(err)
+				reject(new Error("Fatal"));
+				return;
+			}
+			resolve({name: 'Jones'});
+			return;
+		}, ms);
+
+	});
+
+	return promesa;
+
+};
+
+var promesa2 = sleep(1000);
+
+console.log(promesa2);
+
+promesa2.then (function(datos){
+	console.log('promesa terminada', datos);
+}).catch(function(err){
+	console.error("ERROR", err);
+});
